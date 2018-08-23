a5093ce684cf05bec2407e652fa99f27c5de6905
Aien Saidi <aien.saidi@gmail.com>
2018-08-23 22:53:48 +0200

    feat(cors): change allowed hosts

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
+var createError  = require("http-errors");
+var express      = require("express");
+var path         = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var minifyHTML = require("express-minify-html");
+var logger       = require("morgan");
+var minifyHTML   = require("express-minify-html");
 
 var indexRouter = require("./routes/index");
 
@@ -15,50 +15,40 @@ app.set("view engine", "twig");
 
 app.use(logger("dev"));
 app.use(
-  minifyHTML({
-    override: true,
-    exception_url: false,
-    htmlMinifier: {
-      removeComments: true,
-      collapseWhitespace: true,
-      collapseBooleanAttributes: true,
-      removeAttributeQuotes: true,
-      removeEmptyAttributes: true,
-      minifyJS: true,
-    }
-  })
+	minifyHTML({
+		override     : true,
+		exception_url: false,
+		htmlMinifier : {
+			removeComments           : true,
+			collapseWhitespace       : true,
+			collapseBooleanAttributes: true,
+			removeAttributeQuotes    : true,
+			removeEmptyAttributes    : true,
+			minifyJS                 : true,
+		},
+	}),
 );
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({extended: false, limit: "50mb"}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.saidi27.com");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error   = req.app.get("env") === "development" ? err : {};
+	
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
