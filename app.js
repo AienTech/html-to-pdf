ad28a9a029f3739081e031c07f05c8142ece6df3
Aien Saidi <aien.saidi@gmail.com>
2018-10-11 20:17:27 +0200

    fix(static): fix static server for nginx

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: false, limit: "50mb"}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.use(function (req, res, next) {
 	res.setHeader("Access-Control-Allow-Origin", "https://www.saidi27.com");
